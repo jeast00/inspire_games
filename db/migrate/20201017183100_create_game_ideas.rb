627666a38ddab3c82e0137f44e1ffcbf38ef4baf
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-17 14:37:25 -0400

    Added table info for each model
    Added table columns for each model

--- db/migrate/20201017183100_create_game_ideas.rb
+++ db/migrate/20201017183100_create_game_ideas.rb
@@ -1,7 +1,13 @@
 class CreateGameIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :game_ideas do |t|
-
+      t.string :title
+      t.text :description
+      t.string :game_type
+      t.text :beginning_content
+      t.text :middle_content
+      t.text :ending_content
+      
       t.timestamps
     end
   end
