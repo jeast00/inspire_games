4bd7b6b6fbe6cffaebe071e9db22cf6a6b9edb3f
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-17 15:21:50 -0400

    migrated and rolled back databases; added seed data
    tested databases by migrating and adding random seed data and checking first name from user working.  Rolledback databases to referesh and start over with testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+
+User.destroy_all
+GameIdea.destroy_all
+Comment.destroy_all
+
+10.times do 
+    User.create(first_name:Faker::Name.first_name)
+end
