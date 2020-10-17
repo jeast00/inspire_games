981d869c9312cc51261249686557325483e678a6
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-17 15:12:58 -0400

    Added foreign key rows to create comments
    Added the user_id and game_idea_id foreign keys to the create comments table

--- db/migrate/20201017183119_create_comments.rb
+++ db/migrate/20201017183119_create_comments.rb
@@ -2,6 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.text :content
+      t.integer :user_id
+      t.integer :game_idea_id
       t.timestamps
     end
   end
