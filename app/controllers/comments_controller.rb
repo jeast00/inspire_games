4446f47229682676f296cbf9e2fd061c0c9c79ec
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-20 13:37:54 -0400

    Added index method in Comments controller; add index view in views folder; added nested routes for users and comments and users and game_ideas

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+    def index
+        if params[:user_id]
+            @user = User.find(params[:user_id])
+            @comments = @user.comments
+        else
+            @comments = Comment.all 
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :comments
   resources :game_ideas
-  resources :users
+
+  resources :users do
+    resources :comments
+  end
+
+  resources :users do
+    resourcers :game_ideas
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
