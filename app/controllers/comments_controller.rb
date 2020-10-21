31fe469694936cf9e7b2e4560427d15ef5bf1780
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-21 13:38:40 -0400

    Updated comments controller with methods
    Added edit, update, and destroy methods in the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -23,6 +23,25 @@ class CommentsController < ApplicationController
         end
     end
 
+    def edit
+        @comment = Comment.find(params[:id])
+    end
+
+    def update
+        @comment = Comment.find(params[:id])
+
+        if @comment.save
+            redirect_to comments_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        redirect_to comments_path
+    end
 
     private
 
