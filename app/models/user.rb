7dce158e196ab316b4cd9397bedcd87558398d2b
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-22 13:19:02 -0400

    added methods to users controller; updated validations to users model; added index and new views for user to test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :user_name, :first_name, :last_name, :email, :password
+    validates :user_name, :first_name, :last_name, :email, :password, presence: true
+    validates :user_name, :email, uniqueness: true
     has_many :comments
     has_many :game_ideas, through: :comments
 end
