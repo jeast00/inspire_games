7b226bce46d5dbe3d727e1960b089da535c2d01c
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-22 15:23:34 -0400

    Added methods to users controller; added edit view for user
    added rest of CRUD methods for users controller; added an edit view for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
         @users = User.all 
     end
 
+    def show
+        @user = User.find(params[:id])
+    end
+
     def new
         @user = User.new
     end
@@ -16,6 +20,26 @@ class UsersController < ApplicationController
         else
             render :new
         end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        
+        if @user.update(user_params)
+            redirect_to users_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_path
     end
 
 
