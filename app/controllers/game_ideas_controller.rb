6f510033533d2b2b7a7fb2eb27beb19699fef1ff
jeast00 <38165949+jeast00@users.noreply.github.com>
2020-10-22 10:44:35 -0400

    Added methods in game_ideas controller; added a uniqueness validation in game_ideas model
    Added CRUD methods in game_ideas controller; added uniqueness validation for creating a new game idea

--- app/controllers/game_ideas_controller.rb
+++ app/controllers/game_ideas_controller.rb
@@ -4,5 +4,36 @@ class GameIdeasController < ApplicationController
         @game_ideas = GameIdea.all
     end
 
+    def show
+        @game_idea = GameIdea.find(params[:id])
+    end
+
+    def new
+        @game_idea = GameIdea.new
+    end
+
+    def create
+        @game_idea = GameIdea.new(game_idea_params)
+        
+        if @game_idea.save
+            redirect_to game_ideas_path
+        else
+            render :new
+        end
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private 
+
+    def game_idea_params
+        params.require(:game_idea).permit(:title, :description, :game_type, :beginning_content, :middle_content, :ending_content)
+    end
 
 end
--- app/models/game_idea.rb
+++ app/models/game_idea.rb
@@ -1,5 +1,6 @@
 class GameIdea < ApplicationRecord
     validates :title, :description, :game_type, :beginning_content, :middle_content, :ending_content, presence: true
+    validates :title, uniqueness: true
     has_many :comments
     has_many :users, through: :comments
 end
